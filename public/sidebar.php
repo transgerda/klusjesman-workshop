bb2ff39c20f818e4b8de92e572e93a5966d4949e
Babs Berendsen <babsberendsen3@gmail.com>
2025-05-14 09:29:21 +0200

    update

---
+++ public/sidebar.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/sidebar.css">
+<head>
+    <div class="sidenav">
+        <h1>Klusjesman</h1>
+        <hr>
+        <a href="sidebar.php"> Home</a>
+        <a  href="klant_overzicht.php"> Klanten</a>
+        <a href="#"> Afspraken</a>
+        <a href="#"> Facturen</a>
+    </div>
+</head>
