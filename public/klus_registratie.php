52a4b0bfa9e67af88364358ecbcacb30e2111f57
transgerda <info@martijnvanboven.nl>
2025-05-20 10:43:17 +0200

    ff samenvoegen

---
+++ public/klus_registratie.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Klus registrate</h1>
+
+  <form action="post">
+    <label for="naam">Naam klus: </label>
+    <input type="text" name="naam">
+
+    <label for="omschrijving">omschrijving</label>
+    <input type="textarea">
+  </form>
+</body>
+</html>
