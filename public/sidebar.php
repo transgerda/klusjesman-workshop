7e2fc33f3624bfe253366fbd10486a972b95db8d
Zhen-Jelte Wolsink <zhenjelte.wolsink@student.graafschapcollege.nl>
2025-05-28 09:55:13 +0200

    update

--- public/sidebar.php
+++ public/sidebar.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="css/sidebar.css">
 <head>
     <div class="sidenav">
-        <img src="css\logo.png"style="width:450px;height:50%x;zoom:75%;" alt="">
+        <img src="css\logo.png"style="width:350px;height:50%x;padding:15px;" alt="">
         <hr>
         <a href="klant_overzicht.php"> Klanten</a>
         <a href="voorraad_overzicht.php"> Vooraad</a>
